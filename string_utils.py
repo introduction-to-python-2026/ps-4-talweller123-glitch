2c01ff0595451b25a344606d0aeef391ca52bf3f
talweller123-glitch <talweller123@gmail.com>
2025-11-18 11:26:11 +0200

    Update string_utils.py2

--- string_utils.py
+++ string_utils.py
@@ -3,30 +3,23 @@ def split_at_first_digit(formula):
     for i, char in enumerate(formula):
         if char.isdigit():
             first_digit_index = i
-            break
-            
+            break         
     if first_digit_index == -1:
-        return (formula, 1) 
-        
+        return (formula, 1)         
     else:
         prefix = formula[:first_digit_index]
         number_str = formula[first_digit_index:]
-        number_int = int(number_str)
-        
+        number_int = int(number_str)        
         return (prefix, number_int)
-
 def split_before_each_uppercase(formula):
     results = []
-    current_segment = ""
-    
+    current_segment = ""  
     for char in formula:
         if char.isupper() and current_segment:
             results.append(current_segment)
             current_segment = char
         else:
-            current_segment += char
-            
+            current_segment += char            
     if current_segment:
-        results.append(current_segment)
-        
+        results.append(current_segment)        
     return results
